9ff50aad797b675b35d67032f3d7871ae38a6132
Jimin <jimin4124.22@gmail.com>
2024-05-09 08:16:18 +0900

    0509 숫자 야구 게임 랜덤 수 생성

--- src/main/java/exercise/Baseball.java
+++ src/main/java/exercise/Baseball.java
@@ -3,12 +3,14 @@ package exercise;
 import com.example.shoppingmall.utils.Validator;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Baseball {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String answer = "372";
+        Random random = new Random();
+        String answer = generateRandomNumber(random);
         int count = 0;
 
         while (count < 10) {
@@ -19,18 +21,15 @@ public class Baseball {
 
             try {
                 if (Validator.isNumber(Integer.parseInt(input))) {
-                    // 중복된 값을 입력했는지 확인
                     if (guess.length != Arrays.stream(guess).distinct().count()) {
                         System.out.println("중복된 값이 있습니다. 다시 입력해주세요.");
                     }
 
-                    // 3자리 수를 입력했는지 확인
                     if (guess.length != 3) {
                         System.out.println("3자리 수를 입력해주세요.");
                         continue;
                     }
 
-                    // 정답
                     if (Arrays.equals(guess, answer.split(""))) {
                         System.out.println("정답입니다! 축하합니다.");
                         break;
@@ -91,4 +90,22 @@ public class Baseball {
         }
         return ball;
     }
+
+    private static String generateRandomNumber(Random random) {
+        StringBuilder sb = new StringBuilder();
+        boolean[] used = new boolean[10]; // 0부터 9까지의 숫자가 사용되었는지 여부를 저장하는 배열
+
+        // 3자리 숫자 생성
+        for (int i = 0; i < 3; i++) {
+            int digit;
+            do {
+                digit = random.nextInt(10); // 0부터 9까지의 난수 생성
+            } while (used[digit]); // 이미 사용된 숫자인지 확인
+
+            used[digit] = true; // 해당 숫자를 사용됐음으로 표시
+            sb.append(digit); // 숫자를 문자열에 추가
+        }
+        return sb.toString();
+    }
+
 }
